af1244b988516ba66d3e8488560dc4c3d33999f0
Vagan Arakelyan <vra.vagan@gmail.com>
2022-12-22 15:10:21 +0100

    Add C# Code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string[] array = { "Hello", "2", "World", "Dog", "134", "1567", "35", "computer science", "Russia", "Denmark", "Kazan", "Cat" };
+string[] newArray = new string[array.Length];
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("Изначальный массив: ");
+Console.WriteLine("-----------------------------------------------------");
+Console.Write(string.Join(", ", array));
+Console.WriteLine();
+Console.WriteLine("-----------------------------------------------------");
+for(int i=0; i<array.Length; i++) {
+    if(array[i].Length <= 3) {
+        newArray[i] = array[i];
+    }
+}
+newArray = newArray.Where(x => x != null).ToArray();
+Console.WriteLine("Новый массив: ");
+Console.WriteLine("-----------------------------------------------------");
+Console.Write(string.Join(", ", newArray));
+Console.WriteLine();
+Console.WriteLine("-----------------------------------------------------");
